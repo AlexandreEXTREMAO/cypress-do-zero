12d0be5864112707a8e8f01f2848e3a89ac914d0
AlexandreEXTREMAO <alexandrefenix1@hotmail.com>
2025-09-09 20:57:47 -0300

    criando cadastro e preenchendo formulário

--- cypress/e2e/2-testes-serverest/login_serverest.cy.js
+++ cypress/e2e/2-testes-serverest/login_serverest.cy.js
@@ -3,7 +3,19 @@ describe ('Testes de login', () => {
         cy.visit('https://front.serverest.dev/login')
         cy.get('[data-testid="email"]').type('Alexandre@hotmail.com')
         cy.get('[data-testid="senha"]').type('123456789')
-        cy.get('[data-testid="entrar"]').click()
-        cy.contains('Email e/ou senhas inválidos').should('be.visible')    
+        cy.get('[data-testid="entrar"]').click()   
+    })
+})
+
+describe ('Testes de cadastro', () => {
+    it('Criando cadastro e preenchendo forms', () => {
+        cy.visit('https://front.serverest.dev/login')
+        cy.get('[data-testid="cadastrar"]').click()
+        cy.get('[data-testid="nome"]').type('Alexandre')
+        cy.get('[data-testid="email"]').type('Alexandre@hotmail.com')
+        cy.get('[data-testid="password"]').type('123456789')
+        cy.get('[data-testid="checkbox"]').click()
+        cy.get('[data-testid="cadastrar"]').click()
+   
     })
 })
