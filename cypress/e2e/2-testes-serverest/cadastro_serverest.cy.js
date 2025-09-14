0b90ff03371265673737fa731b92766fa45dc7ac
AlexandreEXTREMAO <alexandrefenix1@hotmail.com>
2025-09-14 13:10:39 -0300

    Inserido função para testar o cadastro

---
+++ cypress/e2e/2-testes-serverest/cadastro_serverest.cy.js
@@ -0,0 +1,11 @@
+describe ('Testes de cadastro', () => {
+    it('Criando cadastro e preenchendo forms', () => {
+        cy.visit('https://front.serverest.dev/login')
+        cy.get('[data-testid="cadastrar"]').click()
+        cy.get('[data-testid="nome"]').type('Alexandre')
+        cy.get('[data-testid="email"]').type('Alexandre@hotmail.com')
+        cy.get('[data-testid="password"]').type('123456789')
+        cy.get('[data-testid="checkbox"]').click()
+        cy.get('[data-testid="cadastrar"]').click()   
+    })
+})
--- cypress/e2e/2-testes-serverest/login_serverest.cy.js
+++ cypress/e2e/2-testes-serverest/login_serverest.cy.js
@@ -6,16 +6,3 @@ describe ('Testes de login', () => {
         cy.get('[data-testid="entrar"]').click()   
     })
 })
-
-describe ('Testes de cadastro', () => {
-    it('Criando cadastro e preenchendo forms', () => {
-        cy.visit('https://front.serverest.dev/login')
-        cy.get('[data-testid="cadastrar"]').click()
-        cy.get('[data-testid="nome"]').type('Alexandre')
-        cy.get('[data-testid="email"]').type('Alexandre@hotmail.com')
-        cy.get('[data-testid="password"]').type('123456789')
-        cy.get('[data-testid="checkbox"]').click()
-        cy.get('[data-testid="cadastrar"]').click()
-   
-    })
-})
