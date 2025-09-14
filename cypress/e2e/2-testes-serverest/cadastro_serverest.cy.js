22291c3bce1b07ef4de148a430b99247fa0c4e4b
AlexandreEXTREMAO <alexandrefenix1@hotmail.com>
2025-09-14 13:19:15 -0300

    Testando login com credenciais válidas e inválida

--- cypress/e2e/2-testes-serverest/cadastro_serverest.cy.js
+++ cypress/e2e/2-testes-serverest/cadastro_serverest.cy.js
@@ -1,11 +1,23 @@
 describe ('Testes de cadastro', () => {
-    it('Criando cadastro e preenchendo forms', () => {
+    it('Criando cadastro como administrador', () => {
         cy.visit('https://front.serverest.dev/login')
         cy.get('[data-testid="cadastrar"]').click()
         cy.get('[data-testid="nome"]').type('Alexandre')
         cy.get('[data-testid="email"]').type('Alexandre@hotmail.com')
         cy.get('[data-testid="password"]').type('123456789')
         cy.get('[data-testid="checkbox"]').click()
+        cy.get('[data-testid="cadastrar"]').click()   
+        cy.get('.alert').contains('Este email já está sendo usado')
+    })
+})
+
+describe ('Testes de cadastro', () => {
+    it.only('Criando cadastro como usuário final', () => {
+        cy.visit('https://front.serverest.dev/login')
+        cy.get('[data-testid="cadastrar"]').click()
+        cy.get('[data-testid="nome"]').type('Alexandree')
+        cy.get('[data-testid="email"]').type('Alexandree@hotmail.com')
+        cy.get('[data-testid="password"]').type('12345678')
         cy.get('[data-testid="cadastrar"]').click()   
         cy.get('.alert').contains('Este email já está sendo usado')
     })
--- cypress/e2e/2-testes-serverest/login_serverest.cy.js
+++ cypress/e2e/2-testes-serverest/login_serverest.cy.js
@@ -1,8 +1,18 @@
 describe ('Testes de login', () => {
+    it('Login com credenciais válidas', () => {
+        cy.visit('https://front.serverest.dev/login')
+        cy.get('[data-testid="email"]').type('Alexandree@hotmail.com')
+        cy.get('[data-testid="senha"]').type('12345678')
+        cy.get('[data-testid="entrar"]').click()   
+    })
+})
+
+describe ('Testes de login', () => {
     it('Login com credenciais inválidas', () => {
         cy.visit('https://front.serverest.dev/login')
-        cy.get('[data-testid="email"]').type('Alexandre@hotmail.com')
+        cy.get('[data-testid="email"]').type('Alexandree@hotmail.com')
         cy.get('[data-testid="senha"]').type('123456789')
-        cy.get('[data-testid="entrar"]').click()   
+        cy.get('[data-testid="entrar"]').click()
+        cy.get('.alert').contains('Email e/ou senha inválidos')   
     })
 })
