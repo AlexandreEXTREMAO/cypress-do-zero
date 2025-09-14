a34a15a9f7bb4e059209753bb80c16cd3c476222
AlexandreEXTREMAO <alexandrefenix1@hotmail.com>
2025-09-14 14:21:48 -0300

    Documentando o código de acordo com cada passo

--- cypress/e2e/2-testes-serverest/checkout_serverest.cy.js
+++ cypress/e2e/2-testes-serverest/checkout_serverest.cy.js
@@ -28,16 +28,29 @@ describe ('Testes de login', () => {
     })
 
     it('Adicionando todos os itens ao carrinho', () => {
+        //Visitando o site
         cy.visit('https://front.serverest.dev/login')
+        
+        //logando com sucesso
         cy.get('[data-testid="email"]').type('Alexandree@hotmail.com')
         cy.get('[data-testid="senha"]').type('12345678')
         cy.get('[data-testid="entrar"]').click()
         cy.get('[data-testid="home"]').click()
         cy.wait(2000)
+
+        //Adicionando vários itens ao carrinho
         cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]').click()
         cy.get('[data-testid="adicionar carrinho"]').click()
         cy.get('h1').contains('Em construção aguarde')
         cy.get('[data-testid="home"]').click()
+        cy.get(':nth-child(2) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
+        cy.get('[data-testid="home"]').click()
+        cy.get(':nth-child(3) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
+        cy.get('[data-testid="home"]').click()
+        cy.get(':nth-child(4) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
+        cy.get('[data-testid="home"]').click()
+        cy.get(':nth-child(5) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
+
     })
     
 })
